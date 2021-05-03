756bd0f18e9f7238fe3a89a084fb6063a1a387c9
guisen chen <thecgs001@foxmail.com>
2021-05-03 18:13:06 +0800

    auto commit

---
+++ R_script/棒棒糖图.R
@@ -0,0 +1,7 @@
+library(ggpubr)
+library(ggplot2)
+setwd("C:/Users/22954/Desktop")
+data = read.table("所有免疫细胞相关性.txt",header=T,sep="\t",check.names=F)
+data =na.omit(data)
+data$Group <- as.factor(data$Group)
+ggdotchart(data,y='Correlation',x='Immune_cells_infiltrates',sorting='descending',add='segments',rotate=TRUE,group='Group',ggtheme = theme_pubr(),add.params = list(color = "lightgray", size = 0.5)) +theme(axis.text.y  = element_text(size=5),text = element_text(size=6)) + geom_point(color='black',size=3,,show.legend =T) +geom_point(aes(color=Group),size=2,show.legend =T) + theme(legend.position = 'right')
